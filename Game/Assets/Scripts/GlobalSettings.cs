2e940fc8afbfc3408bf7d1ed9e0981468215d88d
Unknown <e1235813@sina.com>
2013-02-04 00:06:53 +0800

    Updated - Day5 Morning
    Added patrol function in Guard (Untested)
    Added light mark on door and pressplate
    Added Gates of second and third level.

--- Game/Assets/Scripts/GlobalSettings.cs
+++ Game/Assets/Scripts/GlobalSettings.cs
@@ -1,8 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityUtility;
 
 public class GlobalSettings : MonoBehaviour
 {
-	
+	static GlobalSettings _instance;
+	static GlobalSettings Instance
+	{
+		get
+		{
+			if (_instance) return _instance;
+
+			_instance = GlobalObject.GetOrAddComponent<GlobalSettings>();
+			return _instance;
+		}
+	}
+
+	[SerializeField]
+	private float mouseSensitiveness;
+	public static float MouseSensitiveness
+	{
+		get { return Instance.mouseSensitiveness; }
+		set { Instance.mouseSensitiveness = value; }
+	}
+
+	[SerializeField]
+	private bool gotFirstLevelKey;
+	public static bool GotFirstLevelKey
+	{
+		get { return Instance.gotFirstLevelKey; }
+		set { Instance.gotFirstLevelKey = value; }
+	}
+
+	[SerializeField]
+	private bool gotThirdLevelKey;
+	public static bool GotThirdLevelKey
+	{
+		get { return Instance.gotThirdLevelKey; }
+		set { Instance.gotThirdLevelKey = value; }
+	}
+
 }
