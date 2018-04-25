079b475c1086d42704a9f485ec62b1cb404b84f3
LaborcWang <37526788+LaborcWang@users.noreply.github.com>
2018-04-25 13:57:05 +0800

    UI MainMenu and PauseMenu

---
+++ Game/Assets/Scripts/UIScripts/PauseScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    public static bool PauseisActive = false;
+
+    [SerializeField]
+    GameObject PauseMenuUI;
+
+
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseisActive = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseisActive = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseisActive)
+            {
+                Resume();
+                
+            }
+            else
+            {
+                Pause();
+                
+            }
+        }
+
+
+}
+}
